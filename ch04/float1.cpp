a409a0799e4e8f534d2823a5b9905ac95f603e18
Nicolas Garneau <ngarneau@ngarneau.com>
2012-11-03 17:59:31 -0400

    first 3 exemple of this chapter

---
+++ ch04/float1.cpp
@@ -0,0 +1,12 @@
+int integer;
+float floating;
+
+int main()
+{
+    floating = 1.0 / 2.0;           // assign floating 0.5
+    integer = 1 / 3;                // assign integer 0
+    floating = (1/2) + (1/2);       // assign floating 0.0
+    floating = 3.0 / 2.0;           // assign float 1.5
+    integer = floating;             // assign integer 1;
+    return (0);
+}
---
+++ ch04/float2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+float answer;
+
+int main()
+{
+    answer = 1.0/3.0;
+    std::cout << "The value of 1/3 is " << answer << std::endl;
+    return (0);
+}
